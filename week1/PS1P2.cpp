b63d70cb6e1bcc73de0bbd230024d2feb28a7515
akliakkouche <akliakkouche@triton.edu>
2026-01-25 20:13:59 -0600

    Add files via upload

---
+++ week1/PS1P2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // input 
+  int n1, n2 , sum;
+  cout << "Enter two numbers to sum: ";
+  cin >> n1;
+  cin >> n2;
+  // process
+ 
+  sum = n1 + n2;
+  // output 
+  cout << "the sum is :" << sum << endl;
+  return 0;
+}
