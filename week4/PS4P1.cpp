83f473310d3ec03fbfaa8066ed08cf7131b9b7b3
akliakkouche <akliakkouche@triton.edu>
2026-02-15 10:29:48 -0600

    Add files via upload

--- week4/PS4P1.cpp
+++ week4/PS4P1.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    // input 
+    int qtyItem, unitPrice;
+    double extPrice, tax, total;
+
+    cout << "enter quantity of Items :";
+    cin >> qtyItem;
+    
+    // process
+    if(qtyItem >= 1000) {
+        unitPrice = 3;
+    }
+    else unitPrice = 5;
+    
+    extPrice = qtyItem * unitPrice;
+    tax = extPrice * 0.07;
+    total = extPrice + tax;
+
+    //output 
+    cout << "The quantity is :"<< qtyItem << " and the unit price is :" << unitPrice << "the extended price is : " << extPrice << "the tax is :" << tax << "the total is " << total ;
+
+  return 0;
+}
